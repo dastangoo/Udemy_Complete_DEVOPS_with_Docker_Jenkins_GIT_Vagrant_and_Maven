1f420a449e5bdc4f087e4b9fc810aaddfe758ac3
dastangoo <hamed.dastangoo@gmail.com>
2018-10-07 22:29:39 +0330

    Automation Testing Creating Testing xml

--- Selenium_Automation_web/src/Selenium_Automation_web.java
+++ Selenium_Automation_web/src/Selenium_Automation_web.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class Selenium_Automation_web {
 
+	@Test
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\UC Client\\Downloads");
 		WebDriver driver = new ChromeDriver();
